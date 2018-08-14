4d07080315bee0fea21cac477a4edea5a793814c
iainctduncan <iain.duncan@semaphoresolutions.ca>
2018-08-13 18:37:59 -0700

    updated installer

--- photobot_installer.py
+++ photobot_installer.py
@@ -81,10 +81,11 @@ class PhotobotInstaller(object):
 
 
     def install_packages(self):
-        print("running apt-get update, installing nano, vim, gphoto2...")
+        print("running apt-get update, installing nano, vim, git, gphoto2, & gpsd ...")
         self.do("apt-get update")
-        self.do("apt-get install nano vim")
+        self.do("apt-get install nano vim git")
         self.do("apt-get install gphoto2")
+        self.do("apt-get install gpsd")
         if self.confirm("test gphoto2 to see camera? (plug in camera)"):
             self.do("gphoto2 --list-config")
             self.confirm("camera found, continue?", allow_no=False)    
@@ -104,26 +105,31 @@ class PhotobotInstaller(object):
         print("mounting /dev/sda%i" % dev_num)
         self.do("mount dev/sda%i /mnt/usbstorage")
         self.do("chmod 755 /mnt/usbstorage")        
-        if self.confirm("edit fstab file to automount drive as ext3?"):
-            patch = "/dev/sda%i /mnt/usbstorage /ext3 defaults 0 0"
-            # patch the fstab file and test 
-            if not self.args.dry_run:
-                with open(self.fstab_file, "a") as fstab_file:
-                    fstab_file.write(patch)
-            print("Testing fstab file. WARNING: do not reboot if this errors, Pi will hang.")
-            self.do("mount -a")
-            self.confirm("press y to continue, x for exit", allow_no=False)
+        # no longer mounting with fstab, doing it in the init_script
+        #if self.confirm("edit fstab file to automount drive as ext3?"):
+        #    patch = "/dev/sda%i /mnt/usbstorage /ext3 defaults 0 0"
+        #    # patch the fstab file and test
+        #    if not self.args.dry_run:
+        #        with open(self.fstab_file, "a") as fstab_file:
+        #            fstab_file.write(patch)
+        #    print("Testing fstab file. WARNING: do not reboot if this errors, Pi will hang.")
+        #    self.do("mount -a")
+        #    self.confirm("press y to continue, x for exit", allow_no=False)
 
 
     def setup_code(self):
-        print("downloading photobot.py...")
-        self.do("wget https://raw.githubusercontent.com/paddiohara/photobot/master/src/photobot.py")
+        print("cloning photobot repository into /home/pi/photobot
+        self.do("git clone https://github.com/paddiohara/photobot.git /home/pi/photbot")
 
     def setup_directories(self):
         if self.confirm("create directory on pi: /home/pi/captures ?"):
             self.do("mkdir /home/pi/captures")
         if self.confirm("create directory on USB drive: /mnt/usbstorage/captures ?"):
             self.do("mkdir /mnt/usbstorage/captures")
+        if self.confirm("create directory on pi: /home/pi/lorex ?"):
+            self.do("mkdir /home/pi/lorex")
+        if self.confirm("create directory on USB drive: /mnt/usbstorage/lorex ?"):
+            self.do("mkdir /mnt/usbstorage/lorex")
 
 
     def take_test_photo(self):
@@ -138,7 +144,10 @@ class PhotobotInstaller(object):
 
 
     def setup_cron(self):
-        patch = "\n* * * * * root python /home/pi/photobot.py"
+        patch = (
+            "@reboot root /home/pi/photobot/env2/bin/python /home/pi/photobot/src/init_photobot.py"
+            "\n* * * * * root /home/pi/photobot/env2/bin/python /home/pi/photobot/src/photobot.py"
+            "\n* * * * * root python /home/pi/photobot/env2/bin/python /home/pi/photobot/src/photobot_lorex.py")
         if self.confirm("patching %s with patch: '%s'\n?" % (self.cron_file, patch) ):
             if not self.args.dry_run:
                 with open(self.cron_file, "a") as cron_file:
@@ -146,12 +155,16 @@ class PhotobotInstaller(object):
         if self.confirm("reload cron to test? "):
             self.do("restart cron")     
 
+    def setup_supervisor(self):
+        print("Creating symlink at /etc/supervisord/conf.d/ais_receiver.conf")
+        self.do("ln -s /home/pi/photobot/conf/ais_receiver.conf /etc/supervisor/conf.d/ais_receiver.conf")
 
-    def setup_witty(self):
-        print("downloading witty pi installer")
-        self.do("wget http://www.uugear.com/repo/WittyPi/installWittyPi.sh")
-        self.do("sh installWittyPi.sh")
-
+    def setup_python_envs(self):
+        print("Creating python 2 and 3 virtualenvs, and installing dependencies")
+        self.do("virtualenv -p python2 /home/pi/photobot/env2")
+        self.do("/home/pi/photobot/env2/bin/pip install -r /home/pi/photobot/requirements2.txt")
+        self.do("virtualenv -p python3 /home/pi/photobot/env3")
+        self.do("/home/pi/photobot/env3/bin/pip install -r /home/pi/photobot/requirements3.txt")
 
     # main install process
     def main(self):
@@ -169,20 +182,23 @@ class PhotobotInstaller(object):
         if self.confirm("Setup USB drive?"):
             self.setup_drive()
   
-        if self.confirm("Download photobot.py from github?"):
+        if self.confirm("Clone photobot repository?"):
             self.setup_code()
-        
+
+        if self.confirm("Create python environments?"):
+            self.setup_python_envs()
+
         if self.confirm("Create directories for captures?"):
             self.setup_directories()
         
         if self.confirm("Take test photo? (Attach and power on camera before continuing) "):
             self.take_test_photo()
         
-        if self.confirm("Setup cronjob for capture every minute?"):
+        if self.confirm("Setup cronjobs for reboot and capture every minute?"):
             self.setup_cron()
 
-        if self.confirm("Download and install Witty Pi software?"):
-            self.setup_witty()
+        if self.confirm("Create symlink for ais_receiver supervisord conf file?"):
+            self.setup_supervisor()
 
 
         print("\nDONE SETUP\n")
